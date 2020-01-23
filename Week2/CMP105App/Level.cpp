4e894dcee8c199bc0f5cac6e615e1b22d78d0b96
1802616 <angusdale1@gmail.com>
2020-01-23 11:02:43 +0000

    Did INPUT tasks for week 2

--- Week2/CMP105App/Level.cpp
+++ Week2/CMP105App/Level.cpp
@@ -1,4 +1,5 @@
 #include "Level.h"
+#include <math.h> 
 
 Level::Level(sf::RenderWindow* hwnd, Input* in)
 {
@@ -6,7 +7,14 @@ Level::Level(sf::RenderWindow* hwnd, Input* in)
 	input = in;
 
 	// initialise game objects
+	if (!font.loadFromFile("font/arial.ttf")) {
+		std::cout << "Error loading font";
+	}
 
+	text.setFont(font);
+	text.setCharacterSize(24);
+	circle.setRadius(20);
+	circle.setFillColor(sf::Color::Green);
 }
 
 Level::~Level()
@@ -17,8 +25,58 @@ Level::~Level()
 // handle user input
 void Level::handleInput()
 {
+	if (input->isKeyDown(sf::Keyboard::W)) {
+		input->setKeyUp(sf::Keyboard::W);
+		std::cout << "W has been pressed\n";
+	   }
 
+	if (input->isKeyDown(sf::Keyboard::J) && input->isKeyDown(sf::Keyboard::K) && input->isKeyDown(sf::Keyboard::L)) {
+		input->setKeyUp(sf::Keyboard::J);
+		input->setKeyUp(sf::Keyboard::K);
+		input->setKeyUp(sf::Keyboard::L);
+		std::cout << "JKL was pressed\n";
+	}
 
+	if (input->isKeyDown(sf::Keyboard::Escape)) {
+		input->setKeyUp(sf::Keyboard::Escape);
+		window->close();
+	}
+
+	
+	int mousex = input->getMouseX();
+	int mousey = input->getMouseY();
+	text.setString("Mouse: " + std::to_string(mousex) + " " + std::to_string(mousey));
+	
+	bool mouseLPressed = input->isMouseLDown();
+	
+
+	if (mouseLPressed) {
+		if (once) {
+			currentPosX = input->getMouseX();
+			currentPosY = input->getMouseY();
+			once = false;
+		}
+	}
+
+	if (!mouseLPressed && !once) {
+		endPosX = input->getMouseX();
+		endPosY = input->getMouseY();
+
+		int distanceX = endPosX - currentPosX;
+		int distanceY = endPosY - currentPosY;
+
+		float final = sqrt(pow(distanceX, 2) + pow(distanceY, 2));
+		std::cout << final << std::endl;
+		once = true;
+	}
+
+	
+	if (input->isMouseRDown()) {
+		//input->isMouseRDown(false);
+		currentPosX = input->getMouseX();
+		currentPosY = input->getMouseY();
+		circle.setPosition(currentPosX, currentPosY);
+	}
 }
 
 // Update game objects
@@ -31,7 +89,8 @@ void Level::update()
 void Level::render()
 {
 	beginDraw();
-
+	window->draw(text);
+	window->draw(circle);
 	endDraw();
 }
 
--- Week2/CMP105App/Level.h
+++ Week2/CMP105App/Level.h
@@ -10,11 +10,16 @@ class Level{
 public:
 	Level(sf::RenderWindow* hwnd, Input* in);
 	~Level();
+	
+	int currentPosX;
+	int currentPosY;
+	int endPosX;
+	int endPosY;
 
 	void handleInput();
 	void update();
-	void render();
-
+	void render();	
+	
 private:
 	// Default functions for rendering to the screen.
 	void beginDraw();
@@ -25,5 +30,8 @@ private:
 	Input* input;
 
 	// Add your object here
-
+	sf::Text text;
+	sf::Font font;
+	sf::CircleShape circle;
+	bool once = true;
 };
